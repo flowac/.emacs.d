5e0b6a291ed38ad7df8657961671202202da1714
Heman Muresan <hmuresan@ciena.com>
2021-10-05 09:31:39 -0400

    Adding copy over ssh reverse tunnel command to sp00kyWork.

--- sp00kyWork.el
+++ sp00kyWork.el
@@ -1,1 +1,11 @@
 ;; Config specific to work.
+(defun sp00kyWork/copy-to-clipboard ()
+  "Send text from selected region to port 5556. Note, we are expecting this port to be port forwarded
+with something like \"ssh -R 5556:localhost:5556\""
+  (interactive)
+  (if (use-region-p)
+      (let ((regionp (buffer-substring-no-properties (mark) (point))))
+        (shell-command (concat "echo -n " (shell-quote-argument regionp) "| nc localhost 5556"))
+        (call-interactively 'evil-yank))))
+
+(define-key evil-visual-state-map (kbd "y") 'sp00kyWork/copy-to-clipboard)
